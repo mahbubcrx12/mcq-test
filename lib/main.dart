1a7c4fb259c9557f1a413cd6435d33ce4f68de4c
mahbubcrx12 <mahbubul190@gmail.com>
2023-05-23 18:05:38 +0600

    just starting nothing completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mcq_test/screen/splash_screen.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mcq_test/screen/home_page.dart';
 import 'package:mcq_test/widget/custom_button.dart';
 import 'package:mcq_test/widget/custom_textfield.dart';
 import 'package:get/get.dart';
+import 'package:mcq_test/service/auth_controller.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -14,8 +16,29 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-
+  String? errorMessage = "";
+  bool isLogin = true;
   final _formKey = GlobalKey<FormState>();
+
+  Future<void>signInWithEmailAndPassword()async{
+    try{
+      await Auth().signInWithEmailAndPassword(email: _emailController.text, password: _passwordController.text);
+    }on FirebaseAuthException catch(e){
+      setState(() {
+        errorMessage=e.message;
+      });
+    }
+  }
+
+  Future<void> createUserWithEmailAndPassword() async {
+    try{
+      await Auth().createUserWithEmailAndPassword(email: _emailController.text, password: _passwordController.text);
+    } on FirebaseAuthException catch(e){
+      setState(() {
+        errorMessage = e.message;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,17 +69,35 @@ class _LoginPageState extends State<LoginPage> {
               CustomTextField(
                   controller: _passwordController,
                   obscureText: true,
-                  hintText: "Password"),
+                  hintText: "Password",
+              validator: (val){
+                    if(val!.length <5){
+                      Get.snackbar("Password is too short", "Enter at least 5 character");
+                    }
+              },
+              ),
               SizedBox(height: 30,),
               CustomButton(
                 buttonName: "Go",
                 onTap: (){
                   if(_formKey.currentState!.validate()){
                     _formKey.currentState!.save();
-                    //Get.to(()=>HomePage());
-                  }
 
+                  }
+                  Get.to(()=>HomePage());
                 },
+              ),
+              SizedBox(height: 20,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(onPressed: (){}, child: Text("Not registered?Sign Up",style: TextStyle(
+                      color: Colors.redAccent.withOpacity(.5)
+                    ),)),
+                  ],
+                ),
               ),
               Spacer()
             ],
